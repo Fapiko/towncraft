6326921b302c2f941ad0c02a492da6d75d459f7d
Lucas Jandrew <fapiko@gmail.com>
2012-11-13 22:56:20 -0800

    Adding Maven and reorganizing project

--- src/main/com/fapiko/towncraft/TowncraftWindow.java
+++ src/main/com/fapiko/towncraft/TowncraftWindow.java
@@ -34,15 +34,12 @@ public class TowncraftWindow implements KeyListener {
 
 	}
 
-	@Override
 	public void keyTyped(KeyEvent e) {
 	}
 
-	@Override
 	public void keyPressed(KeyEvent e) {
 	}
 
-	@Override
 	public void keyReleased(KeyEvent e) {
 		if ((e.getModifiers() & KeyEvent.CTRL_MASK) != 0) {
 
